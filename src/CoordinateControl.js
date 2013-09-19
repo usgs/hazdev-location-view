ae66590af96f4f8dcde155e7e38930299ff53430
ehunter-usgs <ehunter@usgs.gov>
2013-09-19 14:57:24 -0600

    Create a coordinate control that will accept a latitude and longitude value and update the location object

--- src/CoordinateControl.js
+++ src/CoordinateControl.js
@@ -1,11 +1,109 @@
 /* global define */
 define([
+	'leaflet'
 ], function (
+	L
 ) {
+
 	'use strict';
 
-	var CoordinateControl = function () {
+	var getCoordinateControlMethod = function () {
+		return 'confidence-control';
 	};
 
+	var DEFAULTS = {
+		'location': {
+			'place': null,
+			'longitude': 0,
+			'latitude': 0,
+			'method': getCoordinateControlMethod(),
+			'confidence':null,
+			'accuracy': null
+		},
+		'position': 'topleft'
+	};
+
+	var CoordinateControl =  L.Control.extend({
+		includes: L.Mixin.Events,
+
+		initialize: function (options) {
+			L.Util.setOptions(this, L.Util.extend({}, DEFAULTS, options));
+			this._location = this.options.location || null;
+		},
+
+		onAdd: function (map) {
+
+			this._map = map;
+
+			var control = L.DomUtil.create('div', 'leaflet-coordinate-control');
+			control.title = 'Location Using Latitude/ Longitude';
+			control.innerHTML = [
+					'<form>',
+						'<label for="latitude">Latitude</label>',
+						'<input type="number" name="latitude" id="latitude" min="-90" max="90" />',
+						'<label for="latitude">Longitude</label>',
+						'<input type="number" name="longitude" id="longitude" min="-360" max="360" />',
+						'<button type="submit" id="coordinate-submit">',
+					'</form>'
+			].join('');
+
+			this._latitude = control.querySelector('#latitude');
+			this._longitude = control.querySelector('#longitude');
+
+			// Bind to a submit button click
+			this._submit = control.querySelector('#coordinate-submit');
+			L.DomEvent.addListener(this._submit , 'click', this._onSubmit, this);
+
+			return control;
+		},
+
+		onRemove: function () {
+			this._map = null;
+		},
+
+		setLocation: function (location, options) {
+
+			this._location = location;
+
+			if (options && options.hasOwnProperty('silent') &&
+					options.silent !== true) {
+				this.fire('location', this._location);
+			}
+		},
+
+		getLocation: function () {
+			return this._location;
+		},
+
+		_onSubmit: function () {
+			return this.setLocation(
+					this._getCoordinateLocation(
+							this._latitude.value,
+							this._longitude.value
+					)
+			);
+		},
+
+		_getCoordinateLocation: function(latitude, longitude) {
+			return {
+				'place': null,
+				'longitude': longitude,
+				'latitude': latitude,
+				'method': getCoordinateControlMethod(),
+				'confidence':this._getConfidenceLevel(), // TODO
+				'accuracy': null // TODO
+			};
+		},
+
+		_getConfidenceLevel: function () {
+			return 'confidence';
+		}
+
+	});
+
+	// expose the coordinate control method type
+	CoordinateControl.METHOD = getCoordinateControlMethod();
+
 	return CoordinateControl;
+
 });
--- test/spec/CoordinateControlTest.js
+++ test/spec/CoordinateControlTest.js
@@ -1,13 +1,52 @@
 /* global define, describe, it */
 define([
 	'chai',
-	'CoordinateControl'
+	'CoordinateControl',
+	'leaflet',
+	'sinon'
 ], function (
 	chai,
-	CoordinateControl
+	CoordinateControl,
+	L,
+	sinon
 ) {
 	'use strict';
 	var expect = chai.expect;
+
+	var kingfieldME = {
+		'place': 'test',
+		'longitude': -70.1,
+		'latitude': 45,
+		'method': CoordinateControl.METHOD,
+		'confidence': 'high',
+		'accuracy': 12
+	};
+
+	var boulderCO = {
+		'place': 'test',
+		'longitude': -105.3,
+		'latitude': 40,
+		'method': CoordinateControl.METHOD,
+		'confidence': 'high',
+		'accuracy': 12
+	};
+
+	var control = new CoordinateControl({
+		'location': kingfieldME,
+		'position': 'topleft'
+	});
+
+	var mapDiv = L.DomUtil.create('div', 'map'),
+			baseURL = 'http://{s}.tiles.mapbox.com/v3/bozdoz.map-z05i4vdt/{z}/{x}/{y}.png',
+			base = L.tileLayer(baseURL),
+			map = L.map(mapDiv, {layers: [base]}).setView([54.9682, -112.675], 5);
+
+
+	var getClickEvent = function () {
+		var clickEvent = document.createEvent('MouseEvents');
+		clickEvent.initMouseEvent('click', true, true, window, 1, 0, 0);
+		return clickEvent;
+	};
 
 	describe('CoordinateControl test suite', function () {
 
@@ -17,7 +56,97 @@ define([
 				expect(CoordinateControl).to.not.be.null;
 				/* jshint +W030 */
 			});
+
+			it('Can be instantiated', function () {
+
+				/* jshint -W030 */
+				expect(control.place).to.not.be.null;
+				expect(control.longitude).to.not.be.null;
+				expect(control.latitude).to.not.be.null;
+				expect(control.method).to.not.be.null;
+				expect(control.confidence).to.not.be.null;
+				expect(control.accuracy).to.not.be.null;
+				/* jshint +W030 */
+			});
 		});
+
+
+		describe('Coordinate Control', function () {
+
+			it('Can be added', function () {
+				map.addControl(control);
+				/* jshint -W030 */
+				expect(control._map).to.not.be.null;
+				/* jshint +W030 */
+			});
+
+			it('Can be removed', function () {
+				map.removeControl(control);
+				/* jshint -W030 */
+				expect(control._map).to.be.null;
+				/* jshint +W030 */
+			});
+		});
+
+
+		describe('Location: Latitude/ Longitude', function () {
+
+			it('Can be set', function () {
+
+				control.setLocation(boulderCO);
+				/* jshint -W030 */
+				expect(Number(control._location.latitude)).to.equal(40);
+				expect(Number(control._location.longitude)).to.equal(-105.3);
+				/* jshint +W030 */
+			});
+
+			it('Can get', function () {
+				var location = control.getLocation();
+				/* jshint -W030 */
+				expect(Number(location.latitude)).to.equal(40);
+				expect(Number(location.longitude)).to.equal(-105.3);
+				console.log(location);				/* jshint +W030 */
+			});
+
+			it('can get static METHOD value', function () {
+				expect(CoordinateControl.METHOD).to.equal(control._location.method);
+			});
+
+			it('suppresses notifications with silent option', function () {
+				
+				var onSetLocation = sinon.spy();
+
+				control.on('location', onSetLocation);
+				control.setLocation({}, {silent: true});
+
+				expect(onSetLocation.callCount).to.equal(0);
+			});
+		});
+
+
+		describe('Submit button click', function () {
+
+			it('Updates the location', function () {
+
+				// submit button on form, submits lat/lon values
+				var button = control._submit;
+
+				map.addControl(control);
+
+				control._latitude.value = 55;
+				control._longitude.value = 44;
+
+				button.dispatchEvent(getClickEvent());
+
+				var location = control.getLocation();
+				/* jshint -W030 */
+				expect(Number(location.latitude)).to.equal(55);
+				expect(Number(location.longitude)).to.equal(44);
+				/* jshint +W030 */
+			});
+
+		});
+
 
 	});
 
