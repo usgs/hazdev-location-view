60fb775be7bbcc2fcde78eb5f984b392aa5ddc4b
Hal Simpson <hasimpson@usgs.gov>
2013-09-23 09:20:46 -0600

    Created Utility class for confidence Calculator

--- src/ConfidenceCalculator.js
+++ src/ConfidenceCalculator.js
@@ -4,8 +4,104 @@ define([
 ) {
 	'use strict';
 
-	var ConfidenceCalculator = function () {
+	/**
+	 * Utility class to get the confidence for a location.
+	 * Confidence for a location is 1 to 5.
+	 */
+	var ConfidenceCalculator = {
+
+		/**
+		 * Compute Confidence given latitude and longitude.
+		 * @params latititude {string}
+		 * @params longitude {string}
+		 * latitude and longitude must be strings to keep accuracy.
+		 */
+		'computeFromCoordinates' : function (latitude, longitude) {
+			if( typeof latitude !== 'string' || typeof longitude !== 'string') {
+				return ConfidenceCalculator.NOT_COMPUTED;
+			}
+			var latitudePieces = latitude.split('.');
+			var longitudePieces = longitude.split('.');
+			var latitudeDecimals = latitudePieces[1].length;
+			var longitudeDecimals = longitudePieces[1].length;
+
+			if( latitudeDecimals >= 5 && longitudeDecimals >= 5) {
+				return ConfidenceCalculator.HIGH_CONFIDENCE;
+			}
+			else if( latitudeDecimals >= 4 && longitudeDecimals >= 4) {
+				return ConfidenceCalculator.ABOVE_AVERAGE_CONFIDENCE;
+			}
+			else if( latitudeDecimals >= 3 && longitudeDecimals >= 3) {
+				return ConfidenceCalculator.AVERAGE_CONFIDENCE;
+			}
+			else if( latitudeDecimals >= 2 && longitudeDecimals >= 2) {
+				return ConfidenceCalculator.BELOW_AVERAGE_CONFIDENCE;
+			}
+			else if( latitudeDecimals >= 1 && longitudeDecimals >= 1) {
+				return ConfidenceCalculator.LOW_CONFIDENCE;
+			}
+			else {
+				return ConfidenceCalculator.NOT_COMPUTED;
+			}
+
+		},
+		/**
+		 * Compute Confidence given a zoom level.
+		 * @params zoom {number} indicates the zoom level of the map.
+		 */
+		'computeFromPoint' : function ( zoom ) {
+			if (zoom > 16) {
+				return ConfidenceCalculator.HIGH_CONFIDENCE;
+			} else if (zoom > 14) {
+				return ConfidenceCalculator.ABOVE_AVERAGE_CONFIDENCE;
+			} else if (zoom > 12) {
+				return ConfidenceCalculator.AVERAGE_CONFIDENCE;
+			} else if (zoom > 9) {
+				return ConfidenceCalculator.BELOW_AVERAGE_CONFIDENCE;
+			} else {
+				return ConfidenceCalculator.LOW_CONFIDENCE;
+			}
+		},
+		/**
+		 * Compute Confidence given a geocode result.
+		 * @params result {string}
+		 * Any result other then 'house' returns low confidence.
+		 */
+		'computeFromGeocode' : function (result) {
+			if(result === 'house') {
+				return ConfidenceCalculator.HIGH_CONFIDENCE;
+			} else {
+				return ConfidenceCalculator.LOW_CONFIDENCE;
+			}
+		},
+
 	};
+
+// ----------------------------------------------------------------------
+// Public Static Variables
+// ----------------------------------------------------------------------
+
+/** Constant used to indicate high degree of confidence. */
+ConfidenceCalculator.HIGH_CONFIDENCE =  5;
+    
+/** Constant used to indicate above average confidence. */
+ConfidenceCalculator.ABOVE_AVERAGE_CONFIDENCE =  4;
+
+/** Constant used to indicate moderate degree of confidence. */
+ConfidenceCalculator.AVERAGE_CONFIDENCE =  3;
+
+/** Constant used to indicate below average confidence. */
+ConfidenceCalculator.BELOW_AVERAGE_CONFIDENCE  =  2;
+
+/** Constant used to indicate low degree of confidence. */
+ConfidenceCalculator.LOW_CONFIDENCE  =  1;
+
+/**
+ * Constant used to indicate confidence was not computed or an error occurred
+ * while computing the confidence
+ */
+ConfidenceCalculator.NOT_COMPUTED   = -1;
+
 
 	return ConfidenceCalculator;
 });
--- test/spec/ConfidenceCalculatorTest.js
+++ test/spec/ConfidenceCalculatorTest.js
@@ -19,6 +19,83 @@ define([
 			});
 		});
 
+		describe('computeFromCoordinates', function () {
+			it('Latitude 39.755543 Longitude -105.2210997 returns HIGH_CONFIDENCE',
+				function () {
+				expect(ConfidenceCalculator.computeFromCoordinates(
+					'39.755543',
+					'-105.2210997')
+					).to.equal(ConfidenceCalculator.HIGH_CONFIDENCE);
+			});
+			it('Latitude 39.7554 Longitude -105.2210 return ABOVE_AVERAGE_CONFIDENCE',
+				function () {
+				expect(ConfidenceCalculator.computeFromCoordinates(
+					'39.7554',
+					'-105.2210')
+					).to.equal(ConfidenceCalculator.ABOVE_AVERAGE_CONFIDENCE);
+			});
+			it('Latitude 39.7554 Longitude -105.221 returns AVERAGE_CONFIDENCE',
+				function () {
+				expect(ConfidenceCalculator.computeFromCoordinates(
+					'39.7554',
+					'-105.221')
+					).to.equal(ConfidenceCalculator.AVERAGE_CONFIDENCE);
+			});
+			it('Latitude 39.75 Longitude -105.22 returns BELOW_AVERAGE_CONFIDENCE',
+				function () {
+				expect(ConfidenceCalculator.computeFromCoordinates(
+					'39.75',
+					'-105.22')
+					).to.equal(ConfidenceCalculator.BELOW_AVERAGE_CONFIDENCE);
+			});
+			it('Latitude 39.7 Longitude -105.2 returns LOW_CONFIDENCE',
+				function () {
+				expect(ConfidenceCalculator.computeFromCoordinates(
+					'39.7',
+					'-105.2')
+					).to.equal(ConfidenceCalculator.LOW_CONFIDENCE);
+			});
+			it('Latitude 39.7555 Longitude -105.2210 as numbers returns NOT_COMPUTED',
+				function () {
+				expect(ConfidenceCalculator.computeFromCoordinates(
+					39.755543,
+					-105.2210997)
+					).to.equal(ConfidenceCalculator.NOT_COMPUTED);
+			});
+		});
+		describe('computeFromPoint', function () {
+			it('Zoom > 16', function () {
+				expect(ConfidenceCalculator.computeFromPoint(17)
+					).to.equal(ConfidenceCalculator.HIGH_CONFIDENCE);
+			});
+			it('Zoom > 14', function () {
+				expect(ConfidenceCalculator.computeFromPoint(15)
+					).to.equal(ConfidenceCalculator.ABOVE_AVERAGE_CONFIDENCE);
+			});
+			it('Zoom > 12', function () {
+				expect(ConfidenceCalculator.computeFromPoint(13)
+					).to.equal(ConfidenceCalculator.AVERAGE_CONFIDENCE);
+			});
+			it('Zoom > 9', function () {
+				expect(ConfidenceCalculator.computeFromPoint(10)
+					).to.equal(ConfidenceCalculator.BELOW_AVERAGE_CONFIDENCE);
+			});
+			it('Zoom <= 9', function () {
+				expect(ConfidenceCalculator.computeFromPoint(9)
+					).to.equal(ConfidenceCalculator.LOW_CONFIDENCE);
+			});
+		});
+		describe('computeFromGeocode', function () {
+			it('house', function () {
+				expect(ConfidenceCalculator.computeFromGeocode('house')
+					).to.equal(ConfidenceCalculator.HIGH_CONFIDENCE);
+			});
+			it('Not House', function () {
+				expect(ConfidenceCalculator.computeFromGeocode('Not House')
+					).to.equal(ConfidenceCalculator.LOW_CONFIDENCE);
+			});
+		});
+
 	});
 
 });
