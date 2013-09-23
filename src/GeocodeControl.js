1331453d9b8cc095ccff6b853c3472c004140e7c
Jonathan Brown <jdbrown@usgs.gov>
2013-09-23 16:11:47 -0600

    initial implimtion of geocode control. some test but needs more. waiting on geocoder utility

--- src/GeocodeControl.js
+++ src/GeocodeControl.js
@@ -1,11 +1,73 @@
 /* global define */
 define([
+	'leaflet',
+	'Geocoder'
 ], function (
+	L,
+	Geocoder
 ) {
 	'use strict';
 
-	var GeocodeControl = function () {
+	var DEFAULT_OPTIONS = {
+		position: 'topleft',
+		defaultLocation: null,
+		defaultEnabled: false
 	};
+
+	var GeocodeControl = L.Control.extend({
+		includes: L.Mixin.Events,
+
+		initialize: function (options) {
+			L.Util.setOptions(this, L.Util.extend({}, DEFAULT_OPTIONS, options));
+			this._geocoder = new Geocoder({apiKey: this.options.apiKey});
+		},
+
+		setLocation: function (loc, options) {
+			this._loc = loc;
+
+			if (!(options && options.silent)) {
+				this.fire('location', this._loc);
+			}
+		},
+
+		getLocation: function () {
+			return this._loc;
+		},
+	
+
+		onAdd: function (map) {
+			var container = this._container = L.DomUtil.create('div', 'GeocodeControl'),
+			    textInput = this._textInput = L.DomUtil.create('input', 'GeocodeControl-input', container),
+			    stop = L.DomEvent.stopPropagation;
+			this._map = map;
+
+			L.DomEvent.on(textInput, 'keyup', this._onKeyUp, this);
+			L.DomEvent.on(container, 'keydown', stop);
+			L.DomEvent.on(container, 'keyup', stop);
+			L.DomEvent.on(container, 'keypress', stop);
+			L.DomEvent.on(container, 'mousedown', stop);
+			L.DomEvent.on(container, 'mouseup', stop);
+			L.DomEvent.on(container, 'click', stop);
+
+			return container;
+		},
+ 
+		_doGeocode: function (textAddress) {
+			//ToDo send address out receive lat long back
+			this._geocoder.forward(textAddress, (function (control) {
+				return function (geocodeResult) {
+					control.setLocation(geocodeResult);
+				};
+			})(this));
+		},
+
+		_onKeyUp: function (keyEvent) {
+			if(keyEvent.keyCode === 13) {
+				this._doGeocode(this._textInput.value);
+			}
+		}
+
+	});
 
 	return GeocodeControl;
 });
--- test/spec/GeocodeControlTest.js
+++ test/spec/GeocodeControlTest.js
@@ -1,10 +1,14 @@
 /* global define, describe, it */
 define([
 	'chai',
-	'GeocodeControl'
+	'GeocodeControl',
+	'leaflet',
+	'sinon'
 ], function (
 	chai,
-	GeocodeControl
+	GeocodeControl,
+	L
+	//sinon
 ) {
 	'use strict';
 	var expect = chai.expect;
@@ -19,6 +23,59 @@ define([
 			});
 		});
 
+		describe('initialize()', function () {
+			var G = new GeocodeControl();
+			
+			it('Can be instantiated', function () {
+				expect(G).to.be.an.instanceof(GeocodeControl);
+			});
+
+			it('Inherits from L.Control', function () {
+				expect(G).to.be.an.instanceof(L.Control);
+			});
+
+			it('sets options on itself', function () {
+				expect(G.options).to.be.an.instanceof(Object);
+			});
+
+		});
+
+
+		describe('setLocation()', function () {
+			it('sets its location', function () {
+				var loc = {};
+				var G = new GeocodeControl();
+				G.setLocation(loc);
+				expect(G._loc).to.equal(loc);
+			});
+		});
+
+		describe('getLocation()', function() {
+			it('can get its location', function () {
+				var loc = {};
+				var G = new GeocodeControl();
+				G._loc = loc;
+				expect(G.getLocation()).to.equal(loc);
+			});
+		});
+
+		describe('onAdd()', function () {
+			var G = new GeocodeControl();
+
+			it('Check for map', function () {
+				/* jshint -W030 */
+				expect(G._map).to.not.be.null;
+				/* jshint +W030 */
+			});
+		});
+
+		describe('doGeocode', function (){
+
+		});
+
+		describe('onKeyUp', function (){
+			
+		});
 	});
 
 });
