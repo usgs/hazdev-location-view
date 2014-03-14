30b710b538424c6569304ad54d79b3ef06346e5f
Jonathan Brown <jdbrown@usgs.gov>
2014-03-14 15:10:51 -0600

    added the bind polyfill for phantom js

--- test/index.js
+++ test/index.js
@@ -1,3 +1,36 @@
+'use strict';
+
+// PhantomJS is missing native bind support,
+//     https://github.com/ariya/phantomjs/issues/10522
+// Polyfill from:
+//     https://developer.mozilla.org
+//         /en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
+if (!Function.prototype.bind) {
+	Function.prototype.bind = function (oThis) {
+		if (typeof this !== 'function') {
+			// closest thing possible to the ECMAScript 5 internal IsCallable
+			throw new TypeError('object to be bound is not callable');
+		}
+
+		var aArgs = Array.prototype.slice.call(arguments, 1),
+		    fToBind = this,
+		    NOP = function () {},
+		    fBound;
+
+		fBound = function () {
+			return fToBind.apply(
+					(this instanceof NOP && oThis ? this : oThis),
+					aArgs.concat(Array.prototype.slice.call(arguments)));
+		};
+
+		NOP.prototype = this.prototype;
+		fBound.prototype = new NOP();
+
+		return fBound;
+	};
+}
+
+
 require.config({
 	baseUrl: '..',
 	paths: {
@@ -30,7 +63,6 @@ require([
 ], function (
 	mocha
 ) {
-	'use strict';
 
 	mocha.setup('bdd');
 
