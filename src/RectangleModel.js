679fbdccf0a631cc4995ab22134c4377c4f3bad0
Eric Martinez <emartinez@usgs.gov>
2014-10-14 14:31:43 -0600

    Basic rectangle Model.

---
+++ src/RectangleModel.js
@@ -0,0 +1,34 @@
+/* global define */
+define([
+  'mvc/Model',
+  'util/Util'
+], function (
+  Model,
+  Util
+) {
+  'use strict';
+
+  var DEFAULTS = {
+    north: null,
+    south: null,
+    east: null,
+    west: null
+  };
+
+  var RectangleModel = function (options) {
+    var _this,
+        _initialize;
+
+    options = Util.extend({}, DEFAULTS, options || {});
+
+    _this = Object.create(new Model());
+
+    _initialize = function () {
+    };
+
+    _initialize();
+    return _this;
+  };
+
+  return RectangleModel;
+});
