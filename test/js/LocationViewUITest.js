cce772051a3b4c773ac4ddd2852780846c7fb4a2
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2014-10-28 07:56:10 -0600

    fixed rebase conflicts

--- test/js/LocationViewUITest.js
+++ test/js/LocationViewUITest.js
@@ -1,26 +1,26 @@
 /* global require */
 require([
-	'LocationView'
+  'LocationView'
 ], function (
-	LocationView
+  LocationView
 ) {
-	'use strict';
+  'use strict';
 
-	var _showLocationButton = document.querySelector('#showLocationView'),
-	    _locationResult = document.querySelector('#locationResult'),
-	    _onLocationCallback,
-	    locationView;
+  var _showLocationButton = document.querySelector('#showLocationView'),
+      _locationResult = document.querySelector('#locationResult'),
+      _onLocationCallback,
+      locationView;
 
-	_onLocationCallback = function (loc) {
-		_locationResult.innerHTML = JSON.stringify(loc, null, 2);
-	};
+  _onLocationCallback = function (loc) {
+    _locationResult.innerHTML = JSON.stringify(loc, null, 2);
+  };
 
-	locationView = new LocationView({
-		callback: _onLocationCallback
-	});
+  locationView = new LocationView({
+    callback: _onLocationCallback
+  });
 
-	// Open location view when button is clicked
-	_showLocationButton.addEventListener('click', function () {
-		locationView.show({'location':null});
-	});
+  // Open location view when button is clicked
+  _showLocationButton.addEventListener('click', function () {
+    locationView.show({'location':null});
+  });
 });
