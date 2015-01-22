dd7cfb2d655978e10a6c0e6dd5ba2bf071d767a1
Eric Martinez <emartinez@usgs.gov>
2015-01-22 13:41:48 -0700

    Added linting to test file watch. Fixed watch for source files.

--- gruntconfig/watch.js
+++ gruntconfig/watch.js
@@ -4,12 +4,12 @@ var config = require('./config');
 
 module.exports = {
   scripts: {
-    files: [config.src + '/**/*..js'],
+    files: [config.src + '/**/*.js'],
     tasks: ['jshint:scripts', 'browserify', 'mocha_phantomjs']
   },
   tests: {
     files: [config.test + '/*.html', config.test + '/**/*.js'],
-    tasks: [/*'jshint:tests',*/ 'browserify', 'mocha_phantomjs']
+    tasks: ['jshint:tests', 'browserify', 'mocha_phantomjs']
   },
   gruntfile: {
     files: ['Gruntfile.js'],
