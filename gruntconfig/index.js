a52d078f579773a9f2371a8418e3e2e40120411e
Eric Martinez <emartinez@usgs.gov>
2015-01-26 16:33:36 -0700

    Added clean and copy tasks to gruntconfig.

--- gruntconfig/index.js
+++ gruntconfig/index.js
@@ -3,16 +3,20 @@
 module.exports = (function () {
   var config = {
     browserify: require('./browserify'),
+    clean: require('./clean'),
     compass: require('./compass'),
     connect: require('./connect'),
+    copy: require('./copy'),
     jshint: require('./jshint'),
     mocha_phantomjs: require('./mocha_phantomjs'),
     watch: require('./watch'),
 
     tasks: [
       'grunt-browserify',
+      'grunt-contrib-clean',
       'grunt-contrib-compass',
       'grunt-contrib-connect',
+      'grunt-contrib-copy',
       'grunt-contrib-jshint',
       'grunt-mocha-phantomjs',
       'grunt-contrib-watch'
