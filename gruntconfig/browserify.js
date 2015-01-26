1408dd00af163c065a60e236f999f06ddf35200b
Eric Martinez <emartinez@usgs.gov>
2015-01-26 16:32:17 -0700

    Added bundles for each UI test target.

--- gruntconfig/browserify.js
+++ gruntconfig/browserify.js
@@ -5,6 +5,7 @@ var config = require('./config'),
 
 browserify.options = {
   browserifyOptions: {
+    debug: true,
     paths: [
       process.cwd() + '/' + config.src,
       process.cwd() + '/node_modules/hazdev-webutils/src'
@@ -13,7 +14,17 @@ browserify.options = {
 };
 
 [
-  'index'
+  'index',
+  'js/LocationViewUITest',
+  'js/RegionViewUITest',
+
+  'js/CoordinateControlUITest',
+  'js/GeocodeControlUITest',
+  'js/GeocoderTest',
+  'js/GeolocationControlUITest',
+  'js/LocationControlUITest',
+  'js/PointControlUITest',
+  'js/RectangleControlUITest'
 ].forEach(function (bundle) {
   var targetFile = config.build + '/' + config.test + '/' + bundle + '.js';
   var sourceFile = config.test + '/' + bundle + '.js';
