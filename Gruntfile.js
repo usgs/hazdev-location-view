c835b0d3cef7c8f945c989a79de89b74ea029834
Eric Martinez <emartinez@usgs.gov>
2015-02-03 16:58:48 -0700

    Added logic so tasks only run once per build.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,36 +6,67 @@ module.exports = function (grunt) {
   gruntConfig.tasks.forEach(grunt.loadNpmTasks);
   grunt.initConfig(gruntConfig);
 
-  grunt.registerTask('build', [
+  /**
+   * Checksk whether or not a task has run yet in the current grunt runtime.
+   *
+   * @param task {String} The name of the task to Checksk
+   *
+   * @return True of the task has not yet run, false if the task has already
+   *         run in this grunt runtime.
+   */
+  var taskNotRun = function (task) {
+    try {
+      grunt.task.requires(task);
+      return false; // Task has already run
+    } catch (e) {
+      return true; // Task has not yet run
+    }
+  };
+
+  /**
+   * Creates a task function that executes a configurable list of tasks that
+   * have not yet run in the current grunt runtime.
+   *
+   * @param tasks {Array} An array of task names to potentially run.
+   */
+  var taskList = function (tasks) {
+    return function () {
+      var t = tasks.filter(taskNotRun);
+      grunt.task.run(t);
+    };
+  };
+
+  grunt.registerTask('build', taskList([
     'dev',
     'cssmin',
     'uglify',
     'htmlmin'
-  ]);
+  ]));
 
-  grunt.registerTask('default', [
+  grunt.registerTask('default', taskList([
+    'clean',
     'dev',
-    'test',
     'connect:dev',
+    'test',
     'watch'
-  ]);
+  ]));
 
-  grunt.registerTask('dev', [
+  grunt.registerTask('dev', taskList([
     'browserify',
     'compass',
     'copy',
-  ]);
+  ]));
 
-  grunt.registerTask('dist', [
+  grunt.registerTask('dist', taskList([
     'build',
     'connect:dist'
-  ]);
+  ]));
 
-  grunt.registerTask('test', [
+  grunt.registerTask('test', taskList([
     'dev',
     'connect:test',
     'mocha_phantomjs'
-  ]);
+  ]));
 };
 
 // var mountFolder = function (connect, dir) {
