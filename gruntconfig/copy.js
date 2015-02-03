9415932cecee5db80c368538aaf567ae8fb8162f
Eric Martinez <emartinez@usgs.gov>
2015-02-03 14:41:20 -0700

    Updated copy task per latest discussion.

--- gruntconfig/copy.js
+++ gruntconfig/copy.js
@@ -1,7 +1,6 @@
 'use strict';
 
-var config = require('./config'),
-    node_modules = process.cwd() + '/node_modules';
+var config = require('./config');
 
 var copy = {
   src: {
@@ -16,23 +15,13 @@ var copy = {
     src: ['**/*.html'],
     dest: config.build + '/' + config.test
   },
+
+  // Need images for markers and some controls
   leaflet: {
     expand: true,
-    cwd: node_modules + '/leaflet/dist',
-    src: ['**/leaflet-src.js', '**/leaflet.css', 'images/*'],
-    dest: config.build + '/' + config.src + '/leaflet'
-  },
-  webutils: {
-    expand: true,
-    cwd: node_modules + '/hazdev-webutils/src',
-    src: ['mvc/**/*.css', 'util/**/*.css'],
+    cwd: process.cwd() + '/node_modules/leaflet/dist',
+    src: ['images/*'],
     dest: config.build + '/' + config.src
-  },
-  mocha: {
-    expand: true,
-    cwd: node_modules + '/mocha',
-    src: ['mocha.js', 'mocha.css'],
-    dest: config.build + '/' + config.test + '/mocha'
   }
 };
 
