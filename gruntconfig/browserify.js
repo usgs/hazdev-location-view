89f44f332245858ec2e0f724ed4765eaf9cc44d3
Eric Martinez <emartinez@usgs.gov>
2015-02-03 09:28:38 -0700

    Updated bundling so package exports modules for external use. Examples/tests use this external package.

--- gruntconfig/browserify.js
+++ gruntconfig/browserify.js
@@ -2,6 +2,66 @@
 
 var config = require('./config');
 
+// Modules defined by this (hazdev-location-view) package
+var EXPORTS = [
+  'ConfidenceCalculator',
+  'CoordinateControl',
+  'GeocodeControl',
+  'Geocoder',
+  'GeolocationControl',
+  'LocationControl',
+  'LocationView',
+  'PointControl',
+  'RectangleControl',
+  'RectangleCoordinateView',
+  'RectangleModel',
+  'RectangleOverlayView',
+  'RegionView'
+];
+
+// Modules from other (external) packages
+var EXTERNALS = [
+  'leaflet',
+
+  'mvc/Collection',
+  'mvc/CollectinSelectBox',
+  'mvc/CollectionTable',
+  'mvc/DataTable',
+  'mvc/DownloadView',
+  'mvc/ModalView',
+  'mvc/Model',
+  'mvc/SelectView',
+  'mvc/SortView',
+  'mvc/View',
+  'util/Events',
+  'util/Util',
+  'util/Xhr',
+];
+
+/**
+ * Creates a bundle target configuration.
+ *
+ * @param options Function options see below for keys...
+ *
+ *      bundle       {String}  Name of browserify bundle target.
+ *      destDir      {String}  Name of output directory in which to
+ *                             place built files.
+ *      sourceDir    {String}  Name of input directory to find source
+ *                             files.
+ *
+ * @return A bundle object configuration
+ */
+var makeBundle = function (options) {
+  return {
+    src: options.sourceDir + '/' + options.bundle + '.js',
+    dest: options.destDir + '/' + options.bundle + '.js',
+    options: {
+      external: EXPORTS.concat(EXTERNALS)
+    }
+  };
+};
+
+
 var browserify = {
   options: {
     browserifyOptions: {
@@ -11,28 +71,28 @@ var browserify = {
         process.cwd() + '/node_modules/hazdev-webutils/src'
       ]
     }
-  }
+  },
+
+  // source bundle
+  source: {
+    src: [],
+    dest: config.build + '/' + config.src + '/hazdev-location-view.js',
+    options: {
+      alias: EXPORTS.map(function (path) {
+        return './' + config.src + '/' + path + ':' + path;
+      }),
+      external: EXTERNALS
+    }
+  },
+
+  // test bundle
+  test: makeBundle({
+    bundle: 'index',
+    destDir: config.build + '/' + config.test,
+    sourceDir: config.test
+  })
 };
 
-
-/**
- * Adds a bundle target to Grunt configuration. Modifies global browserify
- * object.
- *
- * @param options Function options see below for keys...
- *
- *      bundle       {String}  Name of browserify bundle target.
- *      destDir      {String}  Name of output directory in which to
- *                             place built files.
- *      sourceDir    {String}  Name of input directory to find source
- *                             files.
- */
-var makeBundle = function (options) {
-  browserify[options.bundle] = {
-    src: options.sourceDir + '/' + options.bundle + '.js',
-    dest: options.destDir + '/' + options.bundle + '.js'
-  };
-};
 
 // bundles for examples
 [
@@ -41,39 +101,17 @@ var makeBundle = function (options) {
   'js/GeocoderTest',
   'js/GeolocationControlUITest',
   'js/LocationControlUITest',
+  'js/LocationViewUITest',
   'js/PointControlUITest',
-  'js/RectangleControlUITest'
+  'js/RectangleControlUITest',
+  'js/RegionViewUITest'
 ].forEach(function (bundle) {
-  makeBundle({
+  browserify[bundle] = makeBundle({
     bundle: bundle,
     destDir: config.build + '/' + config.example,
     sourceDir: config.example
   });
 });
 
-// bundles for tests
-[
-  'index',
-  'js/LocationViewUITest',
-  'js/RegionViewUITest'
-].forEach(function (bundle) {
-  makeBundle({
-    bundle: bundle,
-    destDir: config.build + '/' + config.test,
-    sourceDir: config.test
-  });
-});
-
-// bundles for dist
-[
-  'LocationView',
-  'RegionView'
-].forEach(function (bundle) {
-  makeBundle({
-    bundle: bundle,
-    destDir: config.build + '/' + config.src,
-    sourceDir: config.src
-  });
-});
 
 module.exports = browserify;
