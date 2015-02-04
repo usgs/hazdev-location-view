9d4e2e66965c3c87215f5e335582f3c791f2acf6
Eric Martinez <emartinez@usgs.gov>
2015-02-04 12:24:00 -0700

    Tabs to spaces.

--- test/test.js
+++ test/test.js
@@ -6,51 +6,51 @@
 //     https://developer.mozilla.org
 //         /en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
 if (!Function.prototype.bind) {
-	Function.prototype.bind = function (oThis) {
-		'use strict';
-		if (typeof this !== 'function') {
-			// closest thing possible to the ECMAScript 5 internal IsCallable
-			throw new TypeError('object to be bound is not callable');
-		}
+  Function.prototype.bind = function (oThis) {
+    'use strict';
+    if (typeof this !== 'function') {
+      // closest thing possible to the ECMAScript 5 internal IsCallable
+      throw new TypeError('object to be bound is not callable');
+    }
 
-		var aArgs = Array.prototype.slice.call(arguments, 1),
-		    fToBind = this,
-		    NOP = function () {},
-		    fBound;
+    var aArgs = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        NOP = function () {},
+        fBound;
 
-		fBound = function () {
-			return fToBind.apply(
-					(this instanceof NOP && oThis ? this : oThis),
-					aArgs.concat(Array.prototype.slice.call(arguments)));
-		};
+    fBound = function () {
+      return fToBind.apply(
+          (this instanceof NOP && oThis ? this : oThis),
+          aArgs.concat(Array.prototype.slice.call(arguments)));
+    };
 
-		NOP.prototype = this.prototype;
-		fBound.prototype = new NOP();
+    NOP.prototype = this.prototype;
+    fBound.prototype = new NOP();
 
-		return fBound;
-	};
+    return fBound;
+  };
 }
 
 
 (function () {
-	'use strict';
+  'use strict';
 
-	mocha.setup('bdd');
-	mocha.reporter('html');
+  mocha.setup('bdd');
+  mocha.reporter('html');
 
-	// Add each test class here as they are implemented
-	require('./spec/LocationViewTest');
-	require('./spec/LocationControlTest');
-	require('./spec/GeocodeControlTest');
-	require('./spec/PointControlTest');
-	require('./spec/CoordinateControlTest');
-	require('./spec/ConfidenceCalculatorTest');
-	require('./spec/GeocoderTest');
-	require('./spec/GeolocationControlTest');
+  // Add each test class here as they are implemented
+  require('./spec/LocationViewTest');
+  require('./spec/LocationControlTest');
+  require('./spec/GeocodeControlTest');
+  require('./spec/PointControlTest');
+  require('./spec/CoordinateControlTest');
+  require('./spec/ConfidenceCalculatorTest');
+  require('./spec/GeocoderTest');
+  require('./spec/GeolocationControlTest');
 
-	if (window.mochaPhantomJS) {
-		window.mochaPhantomJS.run();
-	} else {
-		mocha.run();
-	}
+  if (window.mochaPhantomJS) {
+    window.mochaPhantomJS.run();
+  } else {
+    mocha.run();
+  }
 })(this);
