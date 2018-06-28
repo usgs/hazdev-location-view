126ba0aaf4e6ba3a025b1f1c7dea0250b425d14f
ehunter-usgs <ehunter@usgs.gov>
2018-06-28 14:24:56 -0600

    add theme colors to example page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { LocationMapComponent } from 'hazdev-ng-location-view';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
