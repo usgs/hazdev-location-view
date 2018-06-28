90f2d3461994dcbfac8a44c2620d63e820dc553e
ehunter-usgs <ehunter@usgs.gov>
2018-06-28 14:42:12 -0600

    Remove BrowserModule from LocationViewModule

--- projects/hazdev-ng-location-view/src/lib/location-view.module.ts
+++ projects/hazdev-ng-location-view/src/lib/location-view.module.ts
@@ -1,5 +1,4 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
@@ -27,7 +26,6 @@ import { GeocodeService } from './geocode.service';
 @NgModule({
   imports: [
     BrowserAnimationsModule,
-    BrowserModule,
     CommonModule,
     FormsModule,
     HttpClientModule,
